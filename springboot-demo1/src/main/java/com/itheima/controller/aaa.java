f5b7f38015c78be9dde72b6946543360ac3983c7
yonghai <yhaiweirdo@163.com>
2021-12-07 22:18:32 +0800

    有呵呵呵

--- springboot-demo1/src/main/java/com/itheima/controller/aaa.java
+++ springboot-demo1/src/main/java/com/itheima/controller/aaa.java
@@ -2,11 +2,8 @@ package com.itheima.controller;
 
 public class aaa {
     public static void main(String[] args) {
-<<<<<<< HEAD
 		String s = "aaa";
-=======
         String a = "sfs";
->>>>>>> 5fb5b81a91e2a8d8ce8f1262036978dc04591f6d
         System.out.println();
     }
 }
